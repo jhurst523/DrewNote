b09aac69d208cc38a301fc009c06dbb667d4b30f
Jackson Hurst <jhurst@drew.edu>
2015-12-02 16:53:33 -0500

    Changes to DrewNote

---
+++ src/edu/drew/note/BSTreeIterative.java
@@ -0,0 +1,59 @@
+package edu.drew.note;
+
+public class BSTreeIterative implements NoteCollection<T> {
+
+	@Override
+	public boolean add(Note newNote) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Note lookup(long ID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(long ID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Note note) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(Note note) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(long ID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Note[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
